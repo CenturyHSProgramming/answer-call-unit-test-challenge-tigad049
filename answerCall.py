aa9c9f6e824b9c115364a2d46ef2061296330817
Tigas_DavidChristian <tigad049@hsd.k12.or.us>
2023-09-29 09:47:52 -0700

    changing variable names to fit the naming convention

--- answerCall.py
+++ answerCall.py
@@ -8,21 +8,21 @@
 
 # Make sure it returns a value
 
-def answerCall(caller_code: str, sameAreaCode: bool, cur_time: str):
-    shouldAnswerCall = True
+def answerCall(caller_code: str, same_area_code: bool, cur_time: str):
+    should_answer_call = True
 
     cur_time_int = int(cur_time.replace(':', ''))
     if cur_time_int < 700 or cur_time_int > 2200:
-        shouldAnswerCall = False
+        should_answer_call = False
 
-    if sameAreaCode == False:
+    if same_area_code == False:
         if caller_code == "U":
-            shouldAnswerCall = False
+            should_answer_call = False
     
     if caller_code == "T":
-        shouldAnswerCall = False
+        should_answer_call = False
     
-    return shouldAnswerCall
+    return should_answer_call
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
