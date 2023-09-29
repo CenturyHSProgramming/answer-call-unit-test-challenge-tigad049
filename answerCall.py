240fef3dac1a10ca6417270399eebf9616296558
Tigas_DavidChristian <tigad049@hsd.k12.or.us>
2023-09-29 09:38:51 -0700

    removing redundant while check

--- answerCall.py
+++ answerCall.py
@@ -11,19 +11,18 @@
 def answerCall(caller_code: str, sameAreaCode: bool, cur_time: str):
     shouldAnswerCall = True
 
-    while shouldAnswerCall == True:
-        cur_time_int = int(cur_time.replace(':', ''))
-        if cur_time_int < 700 or cur_time_int > 2200:
-            shouldAnswerCall = False
+    cur_time_int = int(cur_time.replace(':', ''))
+    if cur_time_int < 700 or cur_time_int > 2200:
+        shouldAnswerCall = False
 
-        if sameAreaCode == False:
-            if caller_code == "T" or caller_code == "U":
-                shouldAnswerCall = False
-        elif sameAreaCode == True:
-            if caller_code == "T":
-                shouldAnswerCall = False
-        
-        return shouldAnswerCall
+    if sameAreaCode == False:
+        if caller_code == "T" or caller_code == "U":
+            shouldAnswerCall = False
+    elif sameAreaCode == True:
+        if caller_code == "T":
+            shouldAnswerCall = False
+    
+    return shouldAnswerCall
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
