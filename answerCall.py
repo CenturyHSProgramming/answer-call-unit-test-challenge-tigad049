e596d044004b3df2416d200dd03f4e6f4ac1e322
Tigas_DavidChristian <tigad049@hsd.k12.or.us>
2023-09-29 09:48:34 -0700

    cleaning up comments

--- answerCall.py
+++ answerCall.py
@@ -1,12 +1,5 @@
 # answerCall.py
-# by _______
-
-# For instructions on what to do, see README.md
-# or visit (https://github.com/HundredVisionsGuy/answerCall)
-
-# Write function defintion: answerCall()
-
-# Make sure it returns a value
+# by David Tigas
 
 def answerCall(caller_code: str, same_area_code: bool, cur_time: str):
     should_answer_call = True
@@ -25,6 +18,4 @@ def answerCall(caller_code: str, same_area_code: bool, cur_time: str):
     return should_answer_call
 
 if __name__ == '__main__':
-    # Call the function in here if you want to test it
-    # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    pass
