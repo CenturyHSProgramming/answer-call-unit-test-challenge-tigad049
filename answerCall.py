73880278b8fda96e413adfe7079d6705e2ac38f0
Tigas_DavidChristian <tigad049@hsd.k12.or.us>
2023-09-29 09:23:27 -0700

    adding answerCall function - already have some functionality done

--- answerCall.py
+++ answerCall.py
@@ -8,6 +8,19 @@
 
 # Make sure it returns a value
 
+def answerCall(caller_code: str, sameAreaCode: bool, cur_time: str):
+    shouldAnswerCall = True
+
+    while shouldAnswerCall == True:
+        if sameAreaCode == False:
+            if caller_code == "T" or caller_code == "U":
+                shouldAnswerCall = False
+        elif sameAreaCode == True:
+            if caller_code == "T":
+                shouldAnswerCall = False
+        
+        return shouldAnswerCall
+
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
