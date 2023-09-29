53f1dfcb474bdd91cd92daa362be0e54ebc17069
Tigas_DavidChristian <tigad049@hsd.k12.or.us>
2023-09-29 09:06:25 -0700

    fixed pytest tests

--- tests/test_answer_call.py
+++ tests/test_answer_call.py
@@ -1,67 +1,67 @@
 import answerCall as screener
 
 
-def test_answerCallForUnknownSameAreaCodeGoodTime(self):
+def test_answerCallForUnknownSameAreaCodeGoodTime():
     result = screener.answerCall("U", True, "09:00")
     expected = True
     assert result == expected
 
 
-def test_answerCallForUnknownDifferentAreaCodeGoodTime(self):
+def test_answerCallForUnknownDifferentAreaCodeGoodTime():
     result = screener.answerCall("U", False, "14:00")
     expected = False
     assert result == expected
 
 
-def test_answerCallForUnknownSameAreaCodeBadTime(self):
+def test_answerCallForUnknownSameAreaCodeBadTime():
     result = screener.answerCall("U", True, "23:50")
     expected = False
     assert result == expected
 
 
-def test_answerCallForTelemarketerSameAreaCodeGoodTime(self):
+def test_answerCallForTelemarketerSameAreaCodeGoodTime():
     result = screener.answerCall("T", True, "10:40")
     expected = False
     assert result == expected
 
 
-def test_answerCallForTelemarketerDifferentAreaCodeBadTime(self):
+def test_answerCallForTelemarketerDifferentAreaCodeBadTime():
     result = screener.answerCall("T", False, "23:00")
     expected = False
     assert result == expected
 
 
-def test_answerCallForFriendDifferentAreaCodeGoodTime(self):
+def test_answerCallForFriendDifferentAreaCodeGoodTime():
     result = screener.answerCall("F", False, "10:00")
     expected = True
     assert result == expected
 
 
-def test_answerCallForFriendDifferentAreaCodeBadTime(self):
+def test_answerCallForFriendDifferentAreaCodeBadTime():
     result = screener.answerCall("F", False, "23:00")
     expected = False
     assert result == expected
 
 
-def test_answerCallForFriendDifferentAreaCodeGoodTime_2(self):
+def test_answerCallForFriendDifferentAreaCodeGoodTime_2():
     result = screener.answerCall("F", False, "13:00")
     expected = True
     assert result == expected
 
 
-def test_answerCallForRelativeDifferentAreaCodeGoodTime(self):
+def test_answerCallForRelativeDifferentAreaCodeGoodTime():
     result = screener.answerCall("R", False, "9:00")
     expected = True
     assert result == expected
 
 
-def test_answerCallForRelativeDifferentAreaCodeBadTime(self):
+def test_answerCallForRelativeDifferentAreaCodeBadTime():
     result = screener.answerCall("R", False, "04:00")
     expected = False
     assert result == expected
 
 
-def test_answerCallForRelativeDifferentAreaCodeGoodTime_2(self):
+def test_answerCallForRelativeDifferentAreaCodeGoodTime_2():
     result = screener.answerCall("R", False, "16:00")
     expected = True
     assert result == expected
